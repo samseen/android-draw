75fd513f1ee1a0d329cd606f5e37f4f884475cc9
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-07 22:23:03 +0100

    Created a User class.

--- app/src/main/java/com/example/paint/RegisterUser.java
+++ app/src/main/java/com/example/paint/RegisterUser.java
@@ -14,7 +14,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,7 +41,7 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         registerUser.setOnClickListener(this);
 
         editTextFullName = findViewById(R.id.fullName);
-        editTextAge = findViewById(R.id.age);
+        editTextAge = findViewById(R.id.local_government);
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
 
