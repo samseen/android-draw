feb97bcd9e518d668076328aea25c0f1bf450ad9
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-07 22:05:15 +0100

    Updated the login page

--- app/src/main/java/com/example/paint/Login.java
+++ app/src/main/java/com/example/paint/Login.java
@@ -2,13 +2,31 @@ package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
