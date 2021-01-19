1220c95d9396c27c60bc3ce6c29f4098a20a3564
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-19 11:31:05 +0100

    Added variables for the longitude, latitude and text address to the model.

--- app/src/main/java/com/example/paint/Model.java
+++ app/src/main/java/com/example/paint/Model.java
@@ -2,12 +2,33 @@ package com.example.paint;
 
 public class Model {
     private String imageUrl;
+    private String textLatLong;
+    private String textAddress;
+
     public Model() {
 
     }
 
-    public Model(String imageUrl) {
+    public Model(String imageUrl, String textLatLong, String textAddress) {
         this.imageUrl = imageUrl;
+        this.textLatLong = textLatLong;
+        this.textAddress = textAddress;
+    }
+
+    public String getTextLatLong() {
+        return textLatLong;
+    }
+
+    public void setTextLatLong(String textLatLong) {
+        this.textLatLong = textLatLong;
+    }
+
+    public String getTextAddress() {
+        return textAddress;
+    }
+
+    public void setTextAddress(String textAddress) {
+        this.textAddress = textAddress;
     }
 
     public String getImageUrl() {
